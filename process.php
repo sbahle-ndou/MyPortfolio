643a2efe82db2f7d46d72d401c0e2e0ea7b0b224
SBICZA01\A253563 <sbahle.ndou@standardbank.co.za>
2024-04-04 19:22:51 +0200

    Add project files.

---
+++ process.php
@@ -0,0 +1,20 @@
+﻿<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $subject = "Message from My portfolio: $name";
+
+    $to = "sbahle.ndou@standardbank.co.za";
+    $subject = "New message from $name";
+    $body = "Name: $name\nEmail: $email\nMessage: $message";
+
+    if (mail($to, $subject, $body)) {
+        echo "<p>Message sent successfully!</p>";
+    } else {
+        echo "<p>Oops! Something went wrong. Please try again later.</p>";
+    }
+}
+?>
+
